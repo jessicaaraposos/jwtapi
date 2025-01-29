34d6d34c948b0ab5b0bc250fa899b1df3dc04b0d
jessica <jessicaa.raposo@gmail.com>
2025-01-29 09:41:25 -0300

    Create Project JwtAPi

---
+++ src/test/java/com/github/jessicaraposo/jwtapi/JwtapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jessicaraposo.jwtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
